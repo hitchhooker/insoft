5027e15282e6f4d5b58a11ab859d1c7045199cd9
Tommi Niemi <tommi@niemi.lol>
2022-09-27 14:26:07 -0400

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Static Pages
+Route::view('/', 'pages.home')->name('home');
+Route::view('/about', 'pages.about')->name('about');
+Route::view('/recognition', 'pages.recognition')->name('recognition');
+Route::view('/contact', 'pages.contact')->name('contact');
+Route::view('/instructors', 'pages.instructors')->name('instructors');
+Route::view('/cisco-certification-track', 'pages.ciscocert')->name('cisco-certification-track');
+Route::view('/fortinet-training', 'pages.fortinet')->name('fortinet-training');
+Route::view('/iapp-certifications-courses-training', 'pages.iapp')->name('iapp-certifications-courses-training');
+Route::view('/privacy-policy', 'pages.privacy')->name('privacy-policy');
+
+Route::view('/admin', 'admin.home')->name('admin');
+
+
+// for frontend development only
+Route::get('/course', function () {
+    return view('pages.course');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+/*
+Auth::routes();
+*/
+
+/* uncommented
 Route::get('/', function () {
     return view('home');
 });
+*/
+
